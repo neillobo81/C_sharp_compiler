87ef864011d352e3b132e7898149f810c00aa4c5
Neil <neillobo81@gmail.com>
2023-08-19 16:47:42 -0400

    update

--- bb_compiler/Program.cs
+++ bb_compiler/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        while(true){
+            Console.Write("> ");
+            var line = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(line))
+                return;
+            if (line == "1 + 2 * 3")
+                Console.WriteLine("7");
+            else
+                Console.WriteLine("ERROR: Invalid Expression!");
+        }
+    }
+
+
+    enum SyntaxKind{
+
+    }
+
+    class SyntaxToken
+    {
+        public SyntaxToken(SyntaxKind kind, int position, string text)
+        {
+            Kind = kind;
+            Position = position;
+            Text = text;
+        }
+
+        public SyntaxKind Kind { get;}
+        public int Position { get;}
+        public string Text { get;}
+    }
+
+
+    class Lexer{
+        private readonly string _text;
+        private int _position;
+
+        public Lexer(string text){
+            _text = text;
+        }
+
+        public SyntaxToken NextToken()
+        {
+
+        }
     }
 }
